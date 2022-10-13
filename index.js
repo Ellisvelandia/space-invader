df6fc72f82f33f4cd4db242a03592876eed8872d
ellisvelandia <eyis619@gmail.com>
2022-10-12 20:46:35 -0500

    projectiles

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Player {
       y: 0,
     };
 
-    this.rotation = 0
+    this.rotation = 0;
 
     const image = new Image();
     image.src = "./assets/spaceship.png";
@@ -36,7 +36,7 @@ class Player {
       player.position.y + player.height / 2
     );
 
-    c.rotate(this.rotation)
+    c.rotate(this.rotation);
 
     c.translate(
       -player.position.x - player.width / 2,
@@ -49,7 +49,7 @@ class Player {
       this.position.y,
       this.width,
       this.height
-    )
+    );
     c.restore();
   }
 
@@ -61,7 +61,30 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+
+    this.radius = 3;
+  }
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "red";
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+    (this.position.x += this.velocity.x), (this.position.y += this.velocity.y);
+  }
+}
+
 const player = new Player();
+const projectiles = [];
+
 const keys = {
   a: { pressed: false },
   d: { pressed: false },
@@ -73,6 +96,15 @@ function animate() {
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    } else {
+      projectile.update();
+    }
+  });
 
   if (keys.a.pressed && player.position.x >= 0) {
     player.velocity.x = -7;
@@ -94,15 +126,28 @@ animate();
 addEventListener("keydown", ({ key }) => {
   switch (key) {
     case "a":
-      console.log("left");
+      // console.log("left");
       keys.a.pressed = true;
       break;
     case "d":
-      console.log("right");
+      // console.log("right");
       keys.d.pressed = true;
       break;
     case " ":
-      console.log("space");
+      // console.log("space");
+      projectiles.push(
+        new Projectile({
+          position: {
+            x: player.position.x + player.width / 2,
+            y: player.position.y,
+          },
+          velocity: {
+            x: 0,
+            y: -10,
+          },
+        })
+      );
+      // console.log(projectiles);
       break;
   }
 });
@@ -110,15 +155,15 @@ addEventListener("keydown", ({ key }) => {
 addEventListener("keyup", ({ key }) => {
   switch (key) {
     case "a":
-      console.log("left");
+      // console.log("left");
       keys.a.pressed = false;
       break;
     case "d":
-      console.log("right");
+      // console.log("right");
       keys.d.pressed = false;
       break;
-      case " ":
-      console.log("space");
+    case " ":
+      // console.log("space");
       break;
   }
 });
